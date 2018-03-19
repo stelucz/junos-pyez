ec1e9db38e86c066917196188947da1a60bbdd62
Stehlik Lukas <lukas.stehlik@tieto.com>
2018-03-19 08:09:25 +0100

    Add more parameters for pushtemplate.py

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Set of scripts to learn Junos PyEZ automation.
 
 Script loads inventory file, template file and variables file. Renders config file from jinja template and pushes it to device/s listed in inventory file.
 
+Directory structure:
+1. inventories - folder containing inventory files (.yml) - set of devices and their ip address
+1. templates - folder where script is looking for template files (.jinja)
+1. variables - folder where script is looking for variables files (.yml)
+
+
 Required parameters:
 1. inventory - name of the inventory file (without extension) located in inventories folder.
 1. template - name of the template file (without extension) located in templates folder.
@@ -15,3 +21,8 @@ Required parameters:
 Optional parameters:
 1. -dp,--dont-push - skip pushing of config to devices
 1. -dg, --dont-generate - skip generating of config
+1. -u - username
+1. -p - password
+1. -t - ticket/Reason/Commit message
+1. -ct - config type - set, text or xml
+1. -ni - non-interactive processing - yes to all options!! This will automatically commit config.
--- pushtemplate.py
+++ pushtemplate.py
@@ -13,6 +13,12 @@ parser.add_argument('template', help='Name of the template file')
 parser.add_argument('variables', help='Name of the variables file')
 parser.add_argument('-dp', '--dont-push', help='Skip pushing of config to devices', action='store_true')
 parser.add_argument('-dg', '--dont-generate', help='Skip generating of config', action='store_true')
+parser.add_argument('-u', help='Username')
+parser.add_argument('-p', help='Password')
+parser.add_argument('-t', help='Ticket/Reason/Commit message')
+parser.add_argument('-ct', help='Config type - set, text or xml', choices=['set', 'text', 'xml'])
+parser.add_argument('-ni', help='Non-interactive processing - yes to all options!! This will automatically commit '
+                                'config.', action='store_true')
 args, unknown = parser.parse_known_args()
 
 templatePath = os.path.join("templates", args.template + ".jinja")
@@ -23,13 +29,26 @@ devices = loadyaml(inventoryPath)
 cfgnames = []
 
 if not args.dont_push:
-    username = input('Username: ')
-    password = getpass.getpass()
-    ticket_number = input('Ticket: ')
-    print("Config type? \n\t1 - set\n\t2 - text\n\t1 - xml\n\tDefault 1\n")
-    choices = {'1': 'set', '2': 'text', '3': 'xml', 'set': 'set', 'text': 'text', 'xml': 'xml'}
-    slt = input()
-    cfgtype = choices.get(slt, 'set')
+    if not args.u:
+        username = input('Username: ')
+    else:
+        username = args.u
+    if not args.p:
+        password = getpass.getpass()
+    else:
+        password = args.p
+    if not args.t:
+        ticket_number = input('Ticket: ')
+    else:
+        ticket_number = args.t
+    if not args.ct:
+        print("Config type? \n\t1 - set\n\t2 - text\n\t1 - xml\n\tDefault 1\n")
+        choices = {'1': 'set', '2': 'text', '3': 'xml', 'set': 'set', 'text': 'text', 'xml': 'xml'}
+        slt = input()
+        cfgtype = choices.get(slt, 'set')
+    else:
+        cfgtype = args.ct
+
 i = 0
 for dev in devices:
     cfgnames.append(vars['cfg_name'] + "-" + str(i) + '.cfg')
@@ -38,9 +57,10 @@ for dev in devices:
         config = rendercfg(templatePath, vars)
         savecfg(config, cfgnames[i])
     if not args.dont_push:
-        print('You can edit or review configuration file ' + cfgnames[i] + ' before pushing it to device.')
-        push = input('Push configs to device ' + dev + ' at ' + devices[dev]['ip'] + ' ? [Y/n]: ')
-        if push == '' or push.lower() == 'y':
+        if not args.ni:
+            print('You can edit or review configuration file ' + cfgnames[i] + ' before pushing it to device.')
+            push = input('Push configs to device ' + dev + ' at ' + devices[dev]['ip'] + ' ? [Y/n]: ')
+        if push == '' or push.lower() == 'y' or args.ni:
             cfg = loadconfig(cfgnames[i])
             device = Device(host=devices[dev]['ip'], user=username, passwd=password)
             process_configuration(device, cfg, cfgtype, ticket_number, username)
